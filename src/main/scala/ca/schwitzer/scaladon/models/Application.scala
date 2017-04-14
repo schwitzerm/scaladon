851dcd0e83b241160f3fab920f7e570891539dbb
Mitchell Schwitzer <mitchell@schwitzer.ca>
2017-04-13 21:57:51 -0700

    application website field nullable

--- src/main/scala/ca/schwitzer/scaladon/models/Application.scala
+++ src/main/scala/ca/schwitzer/scaladon/models/Application.scala
@@ -4,11 +4,11 @@ import play.api.libs.functional.syntax._
 import play.api.libs.json._
 
 case class Application(name: String,
-                       website: String)
+                       website: Option[String])
 
 object Application {
   implicit val reads: Reads[Application] = (
     (__ \ "name").read[String] and
-    (__ \ "website").read[String]
+    (__ \ "website").readNullable[String]
   )(Application.apply _)
 }
