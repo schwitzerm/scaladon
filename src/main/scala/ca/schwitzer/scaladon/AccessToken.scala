286e9be57568f94f5a95bc3aaa839cdecccf9e3c
Mitchell Schwitzer <mitchell@schwitzer.ca>
2017-04-28 02:31:14 -0700

    import optimization

--- src/main/scala/ca/schwitzer/scaladon/AccessToken.scala
+++ src/main/scala/ca/schwitzer/scaladon/AccessToken.scala
@@ -1,7 +1,5 @@
 package ca.schwitzer.scaladon
 
 import akka.http.javadsl.model.headers.HttpCredentials
-import akka.http.scaladsl.model.headers.Cookie
-import play.api.libs.json._
 
 case class AccessToken(credentials: HttpCredentials)
